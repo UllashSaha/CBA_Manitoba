101adbfefd6e4431bc8532570ec9145b2ff40024
Ullash Saha <UllashS@iqmetrix.com>
2022-04-17 20:14:42 -0500

    phase 1 done small error needs to be fixed

---
+++ Controllers/ContactPresidentController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBook.Controllers
+{
+   //[Authorize]
+    public class ContactPresidentController : Controller
+    {
+        
+        private readonly ILogger<HomeController> _logger;
+
+        public ContactPresidentController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index() => View();
+       
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()  
+            => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        
+    }
+}
